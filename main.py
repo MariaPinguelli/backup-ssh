f2a18e3806701a77afb4a96469d1b87c86262523
MariaPinguelli <mariaferpinguelli@hotmail.com>
2022-12-05 18:15:00 -0300

    deveria funcionar

--- main.py
+++ main.py
@@ -1,40 +1,76 @@
 # Imports
 from os import system
 from random import seed, randint
-from paramiko import RSAKey, SSHClient, AutoAddPolicy
-
-class Processo:
-    def __init__(self, qtdPag):
-        seed()
-        self.id = randint(1000, 9999)
-        self.qtdPag = qtdPag
+from paramiko import SSHClient, AutoAddPolicy 
+from paramiko.auth_handler import AuthenticationException, SSHException
+from scp import SCPClient, SCPException
 
 def main():
     #verificar se o arquivo ssh existe
     #copiar arquivo
     #enviar pra maquina remota
     #inciar processo de copia usando scp
+    commands = [
+        "mkdir ~/uploads"
+    ]
+    
+    filepaths = [
+        "C:/Users/maria/Downloads/teste"
+    ]
 
-    localfilepath = ""
-    remotefilepath = ""
-    ssh_key_filepath = ""
+    local_file_directory = "C:/Users/maria/Downloads/teste"
+    remote_path = "~/uploads"
+    ssh_key_filepath = "~/.ssh/id_rsa.pub"
 
-    hostnameInput = "host"
-    usernameInput = "user"
-    passwordInput = "password"
-    host = ""
-    user = ""
+    host = "127.0.0.1"
+    user = "maria"
+    password = ""
+    
+    # ssh_key = RSAKey.from_private_key_file(ssh_key_filepath)
+    # system(f"ssh-copy-id -i {ssh_key_filepath}.pub {user}@{host}>/dev/null 2>&1")
+    
+    try:
+        client = SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(AutoAddPolicy())
+        client.connect(
+            host,
+            username=user,
+            password=password,
+            key_filename=ssh_key_filepath,
+            timeout=5000,
+        )
+        return client
+    except AuthenticationException as e:
+        print("Erro AuthenticationException a chave SSH foi gerada? " + str(e))
+    except Exception as e:
+        print("Erro: " + str(e))
 
-    ssh_key = RSAKey.from_private_key_file(ssh_key_filepath)
-    system(f"ssh-copy-id -i {ssh_key_filepath}.pub {user}@{host}>/dev/null 2>&1")
+    for cmd in commands:
+        stdin, stdout, stderr = client.exec_command(cmd)
+        stdout.channel.recv_exit_status()
+        response = stdout.readlines()
+        for line in response:
+            print("INPUT: {cmd}\n \ OUTPUT: {line}")
 
-    ssh_client = SSHClient()
-    ssh_client.set_missing_host_key_policy(AutoAddPolicy())
-    ssh_client.connect(hostname = hostnameInput, username = usernameInput, password = passwordInput)
-
-    ftp_client = ssh_client.open_sftp()
-    ftp_client.put(localfilepath, remotefilepath)
-    ftp_client.close()
+    scp = SCPClient(client.get_transport())
+    
+    try:
+        scp.put(
+            local_file_directory,
+            remote_path=remote_path,
+            recursive=True
+        )
+        print("Finished uploading "+ len(filepaths) + " files to " + {remote_path} + " on " + {host})
+    except SCPException as e:
+        print("SCPException during bulk upload: " + str(e))
+    except Exception as e:
+        print("Unexpected exception during bulk upload: " + str(e))
+    
+    if client:
+        client.close()
+    if scp:
+        scp.close()
 
 if __name__ == "__main__":
     main()
